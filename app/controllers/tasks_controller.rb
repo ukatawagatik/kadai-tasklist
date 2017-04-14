9d02f6efbadafc454b2267dc0e62c665bc57d5ac
Taku Kitagawa <33e15006@gmail.com>
2017-04-14 07:46:12 +0000

    Added User Management Functions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
   def index
-    @tasks = Task.order(status: :desc).page(params[:page]).per(5)
+    @user = current_user
+    @task = current_user.tasks.build
+    @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def show
@@ -12,7 +16,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -52,6 +56,13 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  post 'users/create'
+
   root to: 'tasks#index'
   resources :tasks
+  
 end
